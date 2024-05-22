d18e3fcd9508c2910e7fd421e578225bfa2ba692
docoding0228 <npng0228@naver.com>
2024-05-22 19:54:35 +0900

    Cookie and Session

--- src/main/java/com/sparta/springauth/auth/AuthController.java
+++ src/main/java/com/sparta/springauth/auth/AuthController.java
@@ -46,6 +46,7 @@ public class AuthController {
     public String getSession(HttpServletRequest req) {
         // 세션이 존재할 경우 세션 반환, 없을 경우 null 반환
         HttpSession session = req.getSession(false);
+        //
 
         String value = (String) session.getAttribute(AUTHORIZATION_HEADER); // 가져온 세션에 저장된 Value 를 Name 을 사용하여 가져옵니다.
         System.out.println("value = " + value);
